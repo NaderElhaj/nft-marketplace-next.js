43dcd0f3e5c73c32cb97f5285ec8376ed22e92b6
NaderElhaj <naderelhaj@isitc.u-sousse.tn>
2023-03-27 18:06:06 -0700

    Navbar/Footer/Routing Setup/Banner/Top Seller

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Button from './Button';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  Banner,
+  CreatorCard,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+/* eslint-disable import/no-unresolved */
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Footer, Navbar } from '@/components';
+import '@/styles/globals.css';
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <div className="pt-24">
+        <Component {...pageProps} />
+
+      </div>
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/d2f2d01ad3.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default App;
