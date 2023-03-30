f14f3507c886483f4b4959dd203336e0b175521c
NaderElhaj <naderelhaj@isitc.u-sousse.tn>
2023-03-29 17:48:23 -0700

    NFt Context

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,23 @@ import Script from 'next/script';
 
 import { Footer, Navbar } from '@/components';
 import '@/styles/globals.css';
+import { NFTProvider } from '@/context/NFTContext';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-24">
-        <Component {...pageProps} />
+  <NFTProvider>
 
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-24">
+          <Component {...pageProps} />
+
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-    <Script src="https://kit.fontawesome.com/d2f2d01ad3.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/d2f2d01ad3.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default App;
