851f73a6bcb2cf87fc4bd73bbba806ec5d76d044
NaderElhaj <naderelhaj@isitc.u-sousse.tn>
2023-04-04 18:56:13 -0700

    My NFt Page

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,8 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
-    },
-    mainnet: {
-      url: 'https://mainnet.infura.io/v3/8678dc06641e2f6e552fe105e9201d7a',
-      accounts: [privateKey],
-      chainId: 1,
-      gasPrice: 75000000000, // adjust to the current gas price
+      blockGasLimit: 12000000, // set a higher block gas limit
+      autoMine: false,
     },
   },
   solidity: '0.8.4',
