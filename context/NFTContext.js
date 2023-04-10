2de58fd08219d2e87dc2ed63ded24f3c4fc60088
NaderElhaj <naderelhaj@isitc.u-sousse.tn>
2023-04-09 20:25:05 -0700

    Deploying smart contract to live network

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -91,7 +91,7 @@ export const NFTProvider = ({ children }) => {
   const fetchNFTs = async () => {
     setIsLoadingNFT(false);
 
-    const provider = new ethers.providers.JsonRpcProvider();
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.sepolia.dev');
     const contract = fetchContract(provider);
     const data = await contract.fetchMarketItems();
     const items = await Promise.all(data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,16 @@
 require('@nomiclabs/hardhat-waffle');
 const fs = require('fs');
 
-const privateKey = fs.readFileSync('.secret').toString().trim();
-
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
       blockGasLimit: 12000000, // set a higher block gas limit
       autoMine: false,
+    },
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/w3C7zPFLwVtL7jB5h157nSHEUEy1okB6',
+      accounts: ['223daaade006a38deba5441ac4d168cfac02e69987216f1cf9a6a08c0fcc42bd'],
     },
   },
   solidity: '0.8.4',
